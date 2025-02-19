924ec2e41cf75fb3e659ca0ac62adc470d046031
miroku312 <64543733+miroku312@users.noreply.github.com>
2025-02-19 22:59:51 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,52 +3,58 @@ using System.Collections.Generic;
 
 class Program
 {
-    static List<int>[] graph;
-    static int[] subtreeSize;
-    static int removableEdges = 0;
+    static List<int>[] graph;  
+    static bool[] visited;     
+    static int removableEdges = 0; 
 
     static void Main()
     {
-        int n = int.Parse(Console.ReadLine()); 
+        int n = int.Parse(Console.ReadLine());
         if (n % 2 == 1)
         {
-            Console.WriteLine(0); 
+            Console.WriteLine(-1);
             return;
         }
-
-        graph = new List<int>[n + 1];
-        subtreeSize = new int[n + 1];
-
+        graph = new List<int>[n + 1]; 
         for (int i = 1; i <= n; i++)
             graph[i] = new List<int>();
 
         for (int i = 0; i < n - 1; i++)
         {
-            string[] edge = Console.ReadLine().Split();
-            int u = int.Parse(edge[0]);
-            int v = int.Parse(edge[1]);
+            string[] input = Console.ReadLine().Split();
+            int u = int.Parse(input[0]);
+            int v = int.Parse(input[1]);
+
             graph[u].Add(v);
             graph[v].Add(u);
         }
-        DFS(1, -1);
 
+        visited = new bool[n + 1];
+        DFS(1);
         Console.WriteLine(removableEdges);
     }
 
-    static int DFS(int node, int parent)
+    static int DFS(int node)
     {
-        subtreeSize[node] = 1;
+        visited[node] = true;
+        int subtreeSize = 1; 
+
         foreach (int neighbor in graph[node])
         {
-            if (neighbor == parent) continue;
-
-            subtreeSize[node] += DFS(neighbor, node);
+            if (!visited[neighbor])
+            {
+                int childSize = DFS(neighbor);
+                if (childSize % 2 == 0)
+                {
+                    removableEdges++;
+                }
+                else
+                {
+                    subtreeSize += childSize;
+                }
+            }
         }
-        if (subtreeSize[node] % 2 == 0 && parent != -1)
-        {
-            removableEdges++;
-        }
 
-        return subtreeSize[node];
+        return subtreeSize;
     }
 }
